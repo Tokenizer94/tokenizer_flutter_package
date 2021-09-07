499e6f469d25d05c2c8650cef899e6bd1dbc880c
tokenizer94 <mostafafahimi73@gmail.com>
2021-09-08 00:50:38 +0430

    bug fix

--- lib/tokenizer.dart
+++ lib/tokenizer.dart
@@ -4,7 +4,6 @@ import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 part 'platform_widget.dart';
 part 'platform_scaffold.dart';
@@ -12,5 +11,4 @@ part 'platform_appbar.dart';
 part 'platform_button.dart';
 part 'platform_alert_dialog.dart';
 part 'my_base_widget.dart';
-part 'text_style_generator.dart';
 part 'constants.dart';
