afb27c664f3614522ec9a74626902d048539a45c
Tokenizer <mostafafahimi73@gmail.com>
2021-11-14 17:46:07 +0330

    Added some property to PlatformAppbar widget

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,11 +1,11 @@
+## 1.1.1
+
+* Added some fields to [PlatformAppBar] widget.
+
 ## 1.1.0
 
 * Added GestureDetector. Soft keyboard on device is dismissible by
-touching the screen
-
-## 1.0.4
-
-Library is working like a charm in this version
+touching the screen.
 
 ## 0.0.1
 
--- lib/platform_appbar.dart
+++ lib/platform_appbar.dart
@@ -4,11 +4,19 @@ class PlatformAppBar extends PlatformWidget {
   final Widget? leading;
   final Widget title;
   final List<Widget> actions;
+  final double? toolbarHeight;
+  final bool? centerTitle;
+  final Color? backgroundColor;
+  final double? elevation;
 
   const PlatformAppBar({
     required this.title,
     this.leading,
     this.actions = const <Widget>[],
+    this.toolbarHeight,
+    this.centerTitle,
+    this.backgroundColor,
+    this.elevation,
   });
 
   @override
@@ -21,6 +29,7 @@ class PlatformAppBar extends PlatformWidget {
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: actions,
       ),
+      backgroundColor: backgroundColor,
     );
   }
 
@@ -30,6 +39,10 @@ class PlatformAppBar extends PlatformWidget {
       leading: leading,
       title: title,
       actions: actions,
+      toolbarHeight: toolbarHeight,
+      centerTitle: centerTitle,
+      backgroundColor: backgroundColor,
+      elevation: elevation,
     );
   }
 }
